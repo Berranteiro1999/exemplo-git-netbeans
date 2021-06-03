13a9651153b4cf4a2e1ef30c192ac2abe15a0801
Sergio <Sergio@DESKTOP-1MDQ445>
2021-06-03 17:24:07 -0300

    Meu primeiro commit - Sergio Mazer

---
+++ src/main/java/com/mycompany/exemplo/git/netbeans/classepincipal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.exemplo.git.netbeans;
+
+/**
+ *
+ * @author Sergio
+ */
+public class classepincipal {
+    public static void main(String[] args){
+    System.out.println("Teste Git e Netbeans");
+    }
+            
+}
